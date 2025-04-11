ccaefe4144a78fd90fd174f7716cce96b7b13c05
erikaliane <erika.ventura@tecsup.edu.pe>
2025-04-11 17:53:29 -0500

    updated-calidad

--- pages/calidad/notasmodule/index.tsx
+++ pages/calidad/notasmodule/index.tsx
@@ -14,6 +14,7 @@ interface ModuleResult {
   created_at: string;
 }
 
+
 interface Module {
   name: string;
   is_active: boolean;
@@ -21,7 +22,14 @@ interface Module {
   evaluation_id: number;
   created_at: string;
   ModuleResults: ModuleResult[];
-  ResultAuditoria: any[];
+  ResultAuditoria: {
+    user_id :number;
+    module_id: number;
+    cod_llamada: string;
+    resultado: boolean;
+    observaciones: string;
+  }[];
+  
 }
 
 interface Course {
@@ -50,24 +58,29 @@ const NotasIndex: React.FC = () => {
 
   const toggleSidebar = () => setIsDrawerOpen(!isDrawerOpen);
 
+  //validar e hndleToglleNotas
   const handleToggleNotas = (courseId: number) => {
     setSelectedCourseId(selectedCourseId === courseId ? null : courseId);
   };
 
-  // Fetch course data from API
-  useEffect(() => {
-    const fetchCourseData = async () => {
-      try {
-        const response = await fetch(`http://localhost:4100/api/resultado/datos/838/75`); // Replace with actual URL
-        const data = await response.json();
-        setCourseData(data[0]); // Assuming the data structure matches
-      } catch (error) {
-        console.error('Error fetching course data:', error);
-      }
-    };
+  // obtener datos del response 
+ 
+const fetchCourseData = async () => {
+  if (user_id && course_id) {
+    try {
+      const response = await fetch(`http://localhost:4100/api/resultado/datos/${user_id}/${course_id}`);
+      const data = await response.json();
+      setCourseData(data[0]);
+      console.log('Course data:', data[0]);
+    } catch (error) {
+      console.error('Error fetching course data:', error);
+    }
+  }
+};
 
-    fetchCourseData();
-  }, []);
+useEffect(() => {
+  fetchCourseData();
+}, [user_id, course_id]);
 
   const handleOpenModal = (moduleId: number) => {
     setSelectedModuleId(moduleId);
@@ -80,28 +93,53 @@ const NotasIndex: React.FC = () => {
     setSelectedModuleId(null);
   };
 
-  const handleSubmitAuditoria = () => {
-    //no validar observaciones , no es obligatorio
+ 
 
+//envio post a base de datos 
+  const handleSubmitAuditoria = async () => {
     if (codllamada.trim() === '') {
-      alert('Por favor, ingrese el codigo de llamada.');
+      alert('Por favor, ingrese el código de llamada.');
       return;
     }
-
+  
     if (resultado.trim() === '') {
-      alert('Por favor, ingrese un resultado .');
+      alert('Por favor, seleccione un resultado.');
       return;
     }
-    // Aquí iría la lógica para guardar el resultado de auditoría en la base de datos
-    // Por ejemplo, hacer una llamada a la API para registrar la nota de auditoría
-
-    console.log(`Registrando nota de auditoría para el módulo ${selectedModuleId}: ${observaciones}`);
-
-
-
-    // Cerrar el modal después de registrar
-    handleCloseModal();
+  
+    const payload = {
+      user_id: Number(user_id),
+      module_id: selectedModuleId,
+      cod_llamada: codllamada,
+      resultado: resultado === "true", // convertir a boolean
+      observaciones: observaciones
+    };
+  
+    try {
+      const response = await fetch('http://localhost:4100/api/resultado', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+  
+      if (response.ok) {
+        alert('Resultado registrado correctamente');
+        handleCloseModal();
+        setCodLlamada('');
+        setResultado('');
+        setObservaciones('');
+        fetchCourseData(); // Refrescar los datos después de registrar
+      } else {
+        alert('Error al registrar resultado');
+      }
+    } catch (error) {
+      console.error('Error enviando datos:', error);
+      alert('Error en la conexión al servidor');
+    }
   };
+  
 
   return (
     <ProtectedRoute>
@@ -112,43 +150,41 @@ const NotasIndex: React.FC = () => {
           <Sidebar showSidebar={true} setShowSidebar={() => { }} />
         </div>
 
-        <div className="min-h-screen flex flex-col items-center p-6">
+        <div className="min-h-screen flex flex-col items-center p-20">
           <div className="w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
             {courseData && courseData.courseModules.map((module) => (
               <div key={module.module_id} className="bg-white p-6 rounded-xl shadow-lg transition transform hover:scale-105">
                 <h3 className="text-2xl font-bold text-blue-600 mb-4">{module.name}</h3>
+                
                 <div className="space-y-4">
-                  {module.ModuleResults.length > 0 ? (
-                    module.ModuleResults.map((result, index) => (
-                      <div key={index} className="flex justify-between items-center text-lg font-medium">
-                        <span className="text-gray-800">Nota: <span className="font-semibold text-green-500">{result.puntaje}</span></span>
-                      </div>
-                    ))
-                  ) : (
-                    <div className="flex justify-between items-center">
-                      <span className="text-gray-500">Resultado Auditoría: Vacío</span>
-                      <button
-                        className="btn bg-orange-500 hover:bg-orange-600 text-white rounded-lg px-4 py-2 transition-colors"
-                        onClick={() => handleOpenModal(module.module_id)} // Abre el modal cuando se hace clic en el botón
-                      >
-                        Registrar Resultado Auditoría
-                      </button>
-                    </div>
-                  )}
-                </div>
-                <div className="mt-6">
-                  {module.ResultAuditoria.length === 0 && (
-                    <div className="flex justify-between items-center">
-                      <span className="text-gray-500">Resultado Auditoría: Vacío</span>
-                      <button
-                        className="btn bg-indigo-500 hover:bg-indigo-600 text-white rounded-lg px-4 py-2 transition-colors"
-                        onClick={() => handleOpenModal(module.module_id)} // Abre el modal cuando se hace clic en el botón
-                      >
-                        Registrar Resultado Auditoría
-                      </button>
-                    </div>
-                  )}
-                </div>
+  {/* Mostrar las notas si existen */}
+  {module.ModuleResults.length > 0 && module.ModuleResults.map((result, index) => (
+    <div key={index} className="flex justify-between items-center text-lg font-medium">
+      <span className="text-gray-800">
+        Nota: <span className="font-semibold text-green-500">{result.puntaje}</span>
+      </span>
+    </div>
+  ))}
+
+  {/* Mostrar resultado de auditoría si existe */}
+  {module.ResultAuditoria.length > 0 ? (
+    <div className="text-gray-700">
+       <p><strong>Observaciones:</strong> {module.ResultAuditoria[0].cod_llamada|| 'Ninguno'}</p>
+      <p><strong>Resultado Auditoría:</strong> {module.ResultAuditoria[0].resultado ? 'Aprobado' : 'Desaprobado'}</p>
+      <p><strong>Observaciones:</strong> {module.ResultAuditoria[0].observaciones || 'Ninguna'}</p>
+    </div>
+  ) : (
+    // Si no hay resultado de auditoría, mostrar botón para registrar
+    <div className="flex justify-between items-center pt-5">
+      <button
+        className="btn bg-indigo-500 hover:bg-indigo-600 text-white rounded-lg px-4 py-2 transition-colors"
+        onClick={() => handleOpenModal(module.module_id)}
+      >
+        Registrar Resultado Auditoría
+      </button>
+    </div>
+  )}
+</div>
               </div>
             ))}
           </div>
@@ -157,7 +193,10 @@ const NotasIndex: React.FC = () => {
         {/* Modal para registrar la auditoría */}
         {isModalOpen && (
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50">
-            <div className="bg-white p-8 rounded-xl shadow-lg w-96">
+           
+
+            <div className="bg-white p-10 rounded-2xl shadow-md hover:shadow-lg transition-transform transform hover:scale-105 border border-gray-200">
+
               <h3 className="text-xl font-bold mb-4">Registrar Resultado Auditoría</h3>
               
               <input
@@ -167,20 +206,26 @@ const NotasIndex: React.FC = () => {
                 className="w-full p-2 border border-gray-300 rounded mb-4"
                 placeholder="Cod.Llamada"
               />
-              <input
-                type="input"
-                value={resultado}
-                onChange={(e) => setResultado(e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded mb-4"
-                placeholder="Resultado"
-              />
-              <input
-                type="text"
-                value={observaciones}
-                onChange={(e) => setObservaciones(e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded mb-4"
-                placeholder="Observaciones"
-              />
+            
+            <select
+  value={resultado}
+  onChange={(e) => setResultado(e.target.value)}
+  className="w-full p-2 border border-gray-300 rounded mb-4"
+>
+  <option value="">Seleccionar Resultado</option>
+  <option value="true">Aprobado</option>
+  <option value="false">Desaprobado</option>
+</select>
+
+<textarea
+  value={observaciones}
+  onChange={(e) => setObservaciones(e.target.value)}
+  className="w-full p-2 border border-gray-300 rounded mb-4"
+  placeholder="Observaciones (opcional)"
+  rows={3}
+/>
+
+
 
               <div className="flex justify-end space-x-4">
                 <button
