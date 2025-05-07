6485de23e756e5c9c34dea86302a5645a1363614
ceec <administrador.app@ceec.com.pe>
2025-05-07 18:08:36 -0500

    create-evaluation

--- utils/Endpoints.ts
+++ utils/Endpoints.ts
@@ -1,6 +1,6 @@
 //export const baseURL = `https://educa-web-api.onrender.com`;
 
-export const baseURL = `https://educa-web-api.onrender.com`;
+export const baseURL = `http://localhost:4100`;
 
 // authentication
 export const API_AUTH = `${baseURL}/api/auth/signin`;
@@ -41,7 +41,7 @@ export const API_VIDEOS = `${baseURL}/api/videos`;
 export const API_IMAGES = `${baseURL}/api/images`;
 
 //socket
-export const API_SOCKET_URL = `https://educa-web-api.onrender.com`;
+export const API_SOCKET_URL = `http://localhost:4100`;
 
 //perfil
 export const API_PROFILE = `${baseURL}/api/profiles/profiles`;
