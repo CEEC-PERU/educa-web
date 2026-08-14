9e744feb369b3f7ba74ad0a43ee6081ece42ce77
CEEC-PERU <administrador.app@ceec.com.pe>
2026-08-14 15:40:42 -0500

    ocultando contenido que no se usa

--- features/navigation/app-sidebar.supervisor.config.ts
+++ features/navigation/app-sidebar.supervisor.config.ts
@@ -62,31 +62,31 @@ export const supervisorSidebarItems: AppSidebarItem[] = [
   //   icon: AcademicCapIcon,
   //   matchMode: "prefix",
   // },
-  {
-    id: "trainings",
-    label: "Programas de Formación",
-    href: "/supervisor/trainings",
-    icon: TagIcon,
-    children: [
-      {
-        id: "trainings-list",
-        label: "Programas",
-        href: "/supervisor/trainings",
-      },
-      {
-        id: "trainings-assignments",
-        label: "Asignaciones",
-        href: "/supervisor/trainings/assignments",
-      },
-    ],
-  },
-  {
-    id: "materials",
-    label: "Material",
-    href: "/supervisor/materials",
-    icon: ClipboardDocumentCheckIcon,
-    matchMode: "prefix",
-  },
+  //{
+  //  id: "trainings",
+  //  label: "Programas de Formación",
+  //  href: "/supervisor/trainings",
+  //  icon: TagIcon,
+  // children: [
+  //    {
+  //      id: "trainings-list",
+  //      label: "Programas",
+  //      href: "/supervisor/trainings",
+  //    },
+  //    {
+  //      id: "trainings-assignments",
+  //      label: "Asignaciones",
+  //      href: "/supervisor/trainings/assignments",
+  //    },
+  //  ],
+  //},
+  //{
+  //  id: "materials",
+  //  label: "Material",
+  //  href: "/supervisor/materials",
+  //  icon: ClipboardDocumentCheckIcon,
+  //  matchMode: "prefix",
+  //},
   {
     id: "logout",
     label: "Cerrar Sesión",
